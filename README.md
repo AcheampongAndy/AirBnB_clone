625900639aa4c89349d40d0f512e79773cfdf7f4
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-05-11 15:00:37 -0700

    base model

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Welcome to the AirBnB clone project!
+
 First step: Write a command interpreter to manage your AirBnB objects.
---
+++ models/base_model.py
@@ -0,0 +1,49 @@
+#!usr/bin/python3
+"""This is a base model"""
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """
+        args:
+        allows the function to take any number of positional arguments.
+        kwargs:
+        it allows the function to accept any number of keyword arguments.
+        """
+        if kwargs is not NONE and kwargs != {}:
+            for keyword in kwargs:
+                if keyword == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif keyword == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[keyword] = kwargs[keyword]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        """ Returns string representation """
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """ Update the public instance attribute
+        updated_at with the current datetime """
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values """
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = type(self).__name__
+        dictionary["created_at"] = dictionary["created_at"].isoformat()
+        dictionary["updated_at"] = dictionary["updated_at"].isoformat()
+        return dictionary
